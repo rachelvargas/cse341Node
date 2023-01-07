fd0178aaa2dac8cba251b9ec0a17d73b5aa35a47
Rachel Vargas <angeles.vargas@ymail.com>
2023-01-07 10:46:13 -0800

    I have added some file

---
+++ db/connect.js
@@ -0,0 +1,27 @@
+const MongoClient = require('mongodb').MongoClient;
+//const URI = 'mongodb+srv://rachelvargas:mongoRachel@cluster0.ypiaab5.mongodb.net/?retryWrites=true&w=majority';
+let db;
+const connectDB = (callback) => {
+  if(db){
+    console.log("database connected!");
+    return callback(null, db);
+  }
+    MongoClient.connect(process.env.MONGO_URI)
+    .then((client) => {
+      db = client;
+      callback(null, db);
+    })
+    catch((err) => {
+      callback(err);
+    });
+};
+
+const getDb = () => {
+        if (!db) {
+          throw Error('Db not connected!');
+        }
+
+    return db;
+}
+
+module.exports =  {connectDB, getDb}
