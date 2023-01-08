07a8819e60a4dbf7cc53928bec310a26f75d880c
Rachel Vargas <angeles.vargas@ymail.com>
2023-01-07 18:04:24 -0800

    app was modified

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-const mongodb = require('./db/connect.js');
+const mongodb = require('./db/connect');
 const bodyParser = require('body-parser');
 const contactRoutes = require('./routes');
 //const MongoClient = require('mongodb').MongoClient;
 
 //conDB.connectDB();
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
-  });
+  })
   
   app.use('/', contactRoutes);
 
 mongodb.connectDb((err, mongodb) => {
   if (err) {
     console.log(err);
-  } else{
+  } else {
     app.listen(port);
     console.log(`Connected to DB and listening on ${port}`);
   }
